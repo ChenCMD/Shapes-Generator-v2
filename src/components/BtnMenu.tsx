56fb1595b23e273efeca4c30fc610a904a0c6778
ChenCMD <scnmed.g@gmail.com>
2022-01-13 02:47:05 +0900

    :lipstick: SystemMenuをいい感じにした

--- src/components/BtnMenu.tsx
+++ src/components/BtnMenu.tsx
@@ -5,18 +5,19 @@ import Icon, { Icons } from './Icon';
 
 interface BtnMenuProps extends JSX.IntrinsicClassAttributes<HTMLDivElement> {
     icon?: Icons
+    iconSize?: number
     label?: string
     tooltip?: string
     children: JSX.Element | JSX.Element[]
 }
 
-const BtnMenu = ({ icon, label, tooltip, children, ...props }: BtnMenuProps): JSX.Element => {
+const BtnMenu = ({ icon, iconSize, label, tooltip, children, ...props }: BtnMenuProps): JSX.Element => {
     const [active, setActive] = useFocus();
 
     return (
         <div {...props}>
             <Button aria-label={tooltip} onClick={setActive} >
-                {icon && <Icon i={icon} />} {label}
+                {icon && <Icon i={icon} size={iconSize}/>} {label}
             </Button>
             {active && children}
         </div>
--- src/components/SystemMenu.tsx
+++ src/components/SystemMenu.tsx
@@ -1,8 +1,6 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import Button from 'react-bootstrap/esm/Button';
 import Col from 'react-bootstrap/esm/Col';
-import Container from 'react-bootstrap/esm/Container';
-import Row from 'react-bootstrap/esm/Row';
 import { languageMap } from '../locales';
 import styles from '../styles/SystemMenu.module.scss';
 import { isValidateLanguage, SpecificatedLanguage } from '../types/Language';
@@ -23,26 +21,26 @@ const SystemMenu = ({ language, setLanguage, openImportModal, openExportModal }:
     const locale = useLocale();
     const onImport = useCallback(() => openImportModal(true), [openImportModal]);
     const onExport = useCallback(() => openExportModal(true), [openExportModal]);
-    const onLanguageChange = useCallback(
-        ({ target: { value } }: React.MouseEvent<HTMLElement>) => isValidateLanguage(value) && setLanguage(value),
-        [setLanguage]
+    const onLanguageChange = useCallback((lang: string) => isValidateLanguage(lang) && setLanguage(lang), [setLanguage]);
+
+    const languageButtons = useMemo(
+        () => (
+            <div className={styles['button-group']}>{
+                objEntries(languageMap).map(([code, name]) => (
+                    <Button key={code} onClick={() => onLanguageChange(code)} disabled={code === language}>{name}</Button>
+                ))
+            }</div>
+        ),
+        [language, onLanguageChange]
     );
 
     return (
         <div className={styles['window']}>
-            <Container fluid><Row noGutters>
-                <Col>
-                    <div className={styles['nav']}>
-                        <BtnMenu icon="globe" label={locale('menu.language')}>{
-                            objEntries(languageMap).map(([code, name]) => (
-                                <Button key={code} value={code} onClick={onLanguageChange}>{name}</Button>
-                            ))
-                        }</BtnMenu>
-                    </div>
-                </Col>
-                <Col><Button onClick={onImport}><Icon i="download" size={24} /> {locale('menu.import')}</Button></Col>
-                <Col><Button onClick={onExport}><Icon i="upload" size={24} /> {locale('menu.export')}</Button></Col>
-            </Row></Container>
+            <div className={styles['contents']}>
+                <Col><BtnMenu icon="globe" iconSize={24} label={locale('menu.language')}>{languageButtons}</BtnMenu></Col>
+                <Col><Button onClick={onImport}><Icon i="upload" size={24} />{locale('menu.import')}</Button></Col>
+                <Col><Button onClick={onExport}><Icon i="download" size={24} />{locale('menu.export')}</Button></Col>
+            </div>
         </div>
     );
 };
