67913aed12f4c25da61425aabff4d83fbb39c7d6
Ryosuke <korygm33@gmail.com>
2022-01-24 12:07:07 +0000

    :sparkles: diff phaseの検査部分を実装

--- src/sgl/interpreter/errors.ts
+++ src/sgl/interpreter/errors.ts
@@ -1,5 +1,53 @@
-export type DiffPhaseError = never;
+import { ShapeObjectId } from '../definitions/sgl';
+
+/**
+ * 図形オブジェクトへの参照がなぜ正しくないかの理由。
+ * 
+ * 図形オブジェクトへの参照は同期図形にもmodifierのパラメータにも現れるが、
+ * いずれの場合においても、参照が不正な理由は
+ *  - 前方参照(まだ定義されていない図形オブジェクトを参照)しているか
+ *  - 存在しない図形オブジェクトを参照しているか
+ * 
+ * のどちらかである。
+ */
+export type IllFormedReferenceReason = 'forwardReference' | 'notFound';
+
+// #region diff phase のエラー
+
+/**
+ * 同期図形の同期対象への参照が正しく行われていない時のエラー。
+ */
+export interface SyncReferenceIllFormed {
+    __kind: 'SyncReferenceIllFormed';
+    /**
+     * 参照を正しく行っていない同期図形オブジェクトのId
+     */
+    sourceId: ShapeObjectId;
+    reason: IllFormedReferenceReason;
+}
+export const syncReferenceIllFormed =
+    (sourceId: ShapeObjectId, reason: IllFormedReferenceReason): SyncReferenceIllFormed =>
+        ({ __kind: 'SyncReferenceIllFormed', sourceId, reason });
+
+/**
+ * 同じShapeObjectIdが複数の図形オブジェクト定義に使われてしまっている時のエラー。
+ * 
+ * diff phase
+ */
+export interface DuplicateShapeObjectId {
+    __kind: 'DuplicateShapeObjectId';
+    duplicatedId: ShapeObjectId;
+}
+export const duplicateShapeObjectId =
+    (duplicatedId: ShapeObjectId): DuplicateShapeObjectId =>
+        ({ __kind: 'DuplicateShapeObjectId', duplicatedId });
+
+export type DiffPhaseError = SyncReferenceIllFormed | DuplicateShapeObjectId;
+
+// #endregion
+
 export type ModifierTypeCheckPhaseError = never;
+
 export type EvaluationError = never;
 
 export interface NotImplemented {
--- src/sgl/interpreter/interpreter.ts
+++ src/sgl/interpreter/interpreter.ts
@@ -2,8 +2,8 @@ import * as E from 'fp-ts/Either';
 import * as EE from '../../utils/either';
 
 import { pipe } from 'fp-ts/function';
-import { ModifiedShapeObject, SGLProgram, ShapeObjectDefinition } from '../definitions/sgl';
-import { SGLInterpreterError, notImplemented } from './errors';
+import { ModifiedShapeObject, SGLProgram, ShapeObjectDefinition, ShapeObjectId } from '../definitions/sgl';
+import { SGLInterpreterError, notImplemented, DiffPhaseError, duplicateShapeObjectId, syncReferenceIllFormed } from './errors';
 import { SGLEvaluationResult } from './result';
 
 /**
@@ -12,9 +12,13 @@ import { SGLEvaluationResult } from './result';
  * SGLの評価パイプライン上では、まず最初に SGLProgram が DiffPatchedSGLProgram に
  * expandDiff 関数によって「展開」される。この展開処理を diff phase と呼ぶことにする。
  * 
- * diff phase 内では、SGLProgram の中の SynchronizedShapeObject がそれよりも前に定義された
- * ShapeObject を参照していることを検証し、そうでなければエラー、
- * そうであれば SynchronizedShapeObject が保持していた差分を適用する。
+ * diff phase では、SGLProgram の中の各 ShapeObject が
+ *  - 一意なIdを利用しているか
+ *  - SynchronizedShapeObject であれば、同期対象として、
+ *    それよりも前に定義された ShapeObject を参照していること
+ * 
+ * を検査する。検査に通らなければエラー、通れば
+ * SynchronizedShapeObject が保持していた差分を適用する。
  * 
  * 差分適用は単純に Object.assign により行われる。
  * diff phase においては、差分適用の結果がプログラムとして妥当か等の検査は一切しない。
@@ -23,8 +27,35 @@ type DiffPatchedSGLProgram = ShapeObjectDefinition<ModifiedShapeObject>[];
 
 type ErrorOr<A> = E.Either<SGLInterpreterError, A>;
 
-function expandDiff(program: SGLProgram): ErrorOr<DiffPatchedSGLProgram> {
-    return E.left(notImplemented);
+function checkDiffPhaseInput(program: SGLProgram): E.Either<DiffPhaseError, void> {
+    const idSoFar: Set<ShapeObjectId> = new Set();
+    for (const soDef of program) {
+        const { id, shapeObject } = soDef;
+
+        if (idSoFar.has(id)) {
+            return E.left(duplicateShapeObjectId(id));
+        } else if (shapeObject.__kind === 'synchronized' && !idSoFar.has(shapeObject.targetUid)) {
+            const isForwardReference = program.map(sod => sod.id).includes(shapeObject.targetUid);
+            const reason = isForwardReference ? 'forwardReference' : 'notFound';
+            return E.left(syncReferenceIllFormed(id, reason));
+        }
+        idSoFar.add(id);
+    }    
+
+    return E.right(undefined);
+}
+
+function expandCheckedProgram(program: SGLProgram): DiffPatchedSGLProgram {
+    throw Error('Not implemented!');
+}
+
+function expandDiff(program: SGLProgram): E.Either<DiffPhaseError, DiffPatchedSGLProgram> {
+    // 展開に際してエラーが起きないかをまず確認し、大丈夫そうであれば差分を適用していく
+    return pipe(
+        E.right(program),
+        EE.chainTap(checkDiffPhaseInput),
+        E.map(expandCheckedProgram)
+    );
 }
 
 function typeCheckModifiers(program: DiffPatchedSGLProgram): ErrorOr<void> {
