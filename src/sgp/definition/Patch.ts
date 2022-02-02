0b05c7274097f0fdd6594214bbc309c87b2bf3eb
Ryosuke <korygm33@gmail.com>
2022-02-02 17:46:23 +0000

    :recycle: refineAndPatchの型をより正確に

--- src/sgp/definition/Patch.ts
+++ src/sgp/definition/Patch.ts
@@ -7,7 +7,7 @@ abstract class Patch<Bound, TargetType extends Bound> {
   abstract canBeAppliedTo(x: Bound): x is TargetType;
   abstract patch(x: TargetType): TargetType;
 
-  refineAndPatch(x: TargetType): O.Option<TargetType> {
+  refineAndPatch(x: Bound): O.Option<TargetType> {
     return this.canBeAppliedTo(x) ? O.some(this.patch(x)) : O.none;
   }
 }
