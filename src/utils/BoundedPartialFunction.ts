282eb83105cd7ea6015c6a6bad5c0989e5076c06
Ryosuke <korygm33@gmail.com>
2022-02-12 17:23:21 +0000

    :sparkles: SomeBoundedPartialFunctionにasPartialFunctionOnBoundを追加

--- src/utils/BoundedPartialFunction.ts
+++ src/utils/BoundedPartialFunction.ts
@@ -3,8 +3,16 @@ import * as O from 'fp-ts/Option';
 /**
  * `T` に関して存在量化された {@link BoundedPartialFunction}。
  */
-export interface SomeBoundedPartialFunction<Bound> {
-  foldDomain<R>(f: <TargetType extends Bound>(patch: BPartialFn<Bound, TargetType>) => R): R
+export abstract class SomeBoundedPartialFunction<Bound> {
+  abstract foldDomain<R>(f: <TargetType extends Bound>(patch: BPFn<Bound, TargetType>) => R): R;
+
+  /**
+   * このオブジェクトを {@link Bound} 上の部分関数として扱う。
+   */
+  readonly asPartialFunctionOnBound: (x: Bound) => O.Option<Bound> = x =>
+    this.foldDomain<O.Option<Bound>>(<TargetType extends Bound>(patch: BPFn<Bound, TargetType>) =>
+      patch.convertIfApplicable(x)
+    );
 }
 
 export type SomeBPFn<B> = SomeBoundedPartialFunction<B>;
@@ -15,18 +23,18 @@ export type SomeBPFn<B> = SomeBoundedPartialFunction<B>;
  * {@link Bound} に関して反変性を持つ。すなわち、`A extends B` のとき、
  * `BPartialFn<B, T> extends BPartialFn<A, T>` が成り立つ。
  */
-export abstract class BoundedPartialFunction<Bound, T extends Bound> implements SomeBPFn<Bound> {
+export abstract class BoundedPartialFunction<Bound, T extends Bound> extends SomeBoundedPartialFunction<Bound> {
   abstract canBeAppliedTo(x: Bound): x is T;
   abstract convert(x: T): T;
 
   readonly convertIfApplicable = (x: Bound): O.Option<T> =>
     this.canBeAppliedTo(x) ? O.some(this.convert(x)) : O.none;
   
-  readonly foldDomain = <R>(f: <TT extends Bound>(patch: BoundedPartialFunction<Bound, TT>) => R): R =>
+  override readonly foldDomain = <R>(f: <TT extends Bound>(patch: BPFn<Bound, TT>) => R): R =>
     f<T>(this);
 }
 
-export type BPartialFn<B, T extends B> = BoundedPartialFunction<B, T>;
+export type BPFn<B, T extends B> = BoundedPartialFunction<B, T>;
 
 /**
  * {@link T} のコンストラクタの型
@@ -36,7 +44,7 @@ type Constructor<T> = new (...args: never[]) => T;
 /**
  * クラス{@link T}上の関数を{@link BoundedPartialFunction}として扱えるようにする関数。
  */
-export const definePartialFnOnClass = <T>(clazz: Constructor<T>, f: (x: T) => T): BoundedPartialFunction<unknown, T> =>
+export const definePartialFnOnClass = <T>(clazz: Constructor<T>, f: (x: T) => T): BPFn<unknown, T> =>
   new class extends BoundedPartialFunction<unknown, T> {
     canBeAppliedTo(x: unknown): x is T {
       return x instanceof clazz;
