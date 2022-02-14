ca631af12b86c769e02e927ccc518fc55209c040
Ryosuke <korygm33@gmail.com>
2022-02-14 19:32:08 +0000

    :sparkles: typecheck phaseを実装する

--- src/sgp/interpreter/phases/typecheck.ts
+++ src/sgp/interpreter/phases/typecheck.ts
@@ -1,8 +1,75 @@
 import * as E from 'fp-ts/Either';
+import * as EE from '../../../utils/either';
+import * as RA from 'fp-ts/ReadonlyArray';
+import { subsetOf } from '../../../utils/ReadonlySet';
 
 import { DiffPatchedSGP } from './diff-expansion';
-import { InterpreterErrorOr, notImplemented } from '../errors';
+import { InterpreterErrorOr, modifierRequirementFailed, ModifierTypeCheckPhaseError, modifierTypeError } from '../errors';
+import { pipe } from 'fp-ts/function';
+import { ModifiedShapeDefinition } from '../../definition/SGP';
+import { ShapeObjectDefinitionUid } from '../../definition/Uid';
+
+type TypeCheckErrorOrVoid = E.Either<ModifierTypeCheckPhaseError, void>;
+
+function typeCheckPipeline(def: ModifiedShapeDefinition): TypeCheckErrorOrVoid {
+  const { definitionUid: shapeDefinitionUid, shapeObject } = def;
+
+  let overallOutput = shapeObject.shape.outputSpec;
+  for (const { definitionUid: modifierDefinitionUid, modifier } of shapeObject.modifiers) {
+    const output = modifier.outputSpec(overallOutput);
+    if (output._tag === 'Left') {
+      return E.left(modifierTypeError(shapeDefinitionUid, modifierDefinitionUid));
+    } else {
+      overallOutput = output.right;
+    }
+  }
+
+  return E.right(undefined);
+}
+
+function typeCheckPipelinesOfProgram(program: DiffPatchedSGP): TypeCheckErrorOrVoid {
+  return pipe(
+    program,
+    RA.traverse(E.Applicative)(typeCheckPipeline),
+    EE.as(undefined)
+  );
+}
+
+function typeCheckModifierRequirements(definitionsSoFar: ReadonlySet<ShapeObjectDefinitionUid>, shape: ModifiedShapeDefinition): TypeCheckErrorOrVoid {
+  return pipe(
+    shape.shapeObject.modifiers,
+    RA.traverse(E.Applicative)(({ definitionUid: modifierDefinitionUid, modifier }) =>
+      pipe(
+        modifier.partialEvaluationResultRequirements(),
+        subsetOf(definitionsSoFar),
+        (isSubset: boolean) => isSubset
+          ? E.right(undefined)
+          : E.left(modifierRequirementFailed(shape.definitionUid, modifierDefinitionUid))
+      )
+    ),
+    EE.as(undefined)
+  );
+}
+
+function typeCheckModifierRequirementsOfProgram(program: DiffPatchedSGP): TypeCheckErrorOrVoid {
+  const definitionsSoFar: Set<ShapeObjectDefinitionUid> = new Set();
+  for (const def of program) {
+    const checkResult = typeCheckModifierRequirements(definitionsSoFar, def);
+    if (checkResult._tag === 'Left') {
+      return checkResult;
+    } else {
+      definitionsSoFar.add(def.definitionUid);
+    }
+  }
+
+  return E.right(undefined);
+}
 
 export function typeCheckModifiers(program: DiffPatchedSGP): InterpreterErrorOr<void> {
-  return E.left(notImplemented);
+  return pipe(
+    E.right(program),
+    EE.chainTap(typeCheckModifierRequirementsOfProgram),
+    EE.chainTap(typeCheckPipelinesOfProgram),
+    EE.as(undefined)
+  );
 }
