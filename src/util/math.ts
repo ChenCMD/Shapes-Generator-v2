d0e524cd238351fc130597479a547e33c0ab5a32
Ryosuke Kondo <korygm33@gmail.com>
2022-10-02 09:56:51 +0700

    :art: `spreadPointsOverPath` をリファクタリングし、ドキュメントも書く

--- src/util/math.ts
+++ src/util/math.ts
@@ -1,4 +1,6 @@
 import { Point } from './types/Point';
+import { Vector2D } from './types/Vector2D';
+import * as E from 'fp-ts/Either';
 
 export function toRadians(degree: number): number {
   return degree * (Math.PI / 180);
@@ -31,11 +33,11 @@ export function rotateMatrix2D({ x, y }: Point, rotation: number): Point {
   };
 }
 
-interface SampledPoint {
-  t: number;
-  x: number;
-  y: number;
+export interface ParameterizedPoint {
+  parameter: number;
+  point: Vector2D;
 }
+
 /**
  * 連続関数 `(xAt, yAt)` の `[0, 1]` 区間上をサンプリングする。
  * 与えられた関数が連続でない場合処理の終了が保証されない。
@@ -48,19 +50,26 @@ interface SampledPoint {
  *    - `p2.t - p1.t < minimumDomainGap`
  */
 export function sampleDensely(
-  pointAt: (t: number) => { x: number; y: number },
+  pointAt: (parameter: number) => Vector2D,
   maximumRangeGap = 0.1,
   minimumDomainGap = 0.001
-): SampledPoint[] {
-  const sampleAt = (t: number) => ({ t, ...pointAt(t) });
-  const sufficientlyClose = (p1: SampledPoint, p2: SampledPoint) =>
-    (p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2 < maximumRangeGap ** 2 ||
-    Math.abs(p1.t - p2.t) < minimumDomainGap;
-  const midTOf = (p1: SampledPoint, p2: SampledPoint) => (p1.t + p2.t) / 2.0;
+): ParameterizedPoint[] {
+  const sampleAt = (parameter: number): ParameterizedPoint => ({
+    parameter,
+    point: pointAt(parameter)
+  });
 
-  const results: SampledPoint[] = [sampleAt(0.0)];
+  const sufficientlyClose = (p1: ParameterizedPoint, p2: ParameterizedPoint) =>
+    (p2.point.x - p1.point.x) ** 2 + (p2.point.y - p1.point.y) ** 2 <
+      maximumRangeGap ** 2 ||
+    Math.abs(p1.parameter - p2.parameter) < minimumDomainGap;
+
+  const midTOf = (p1: ParameterizedPoint, p2: ParameterizedPoint) =>
+    (p1.parameter + p2.parameter) / 2.0;
+
+  const results: ParameterizedPoint[] = [sampleAt(0.0)];
   let nextSampleT: number | undefined = 1.0;
-  const sampledAhead: SampledPoint[] = [];
+  const sampledAhead: ParameterizedPoint[] = [];
 
   while (true) {
     const lastResult = results[results.length - 1];
@@ -90,31 +99,55 @@ export function sampleDensely(
   }
 }
 
-export function spreadSamplesOver(
-  samples: SampledPoint[],
+/**
+ * {@link pathNodes} で定義されるパス上に {@link interpolatedPoints} 個の
+ * 点を、パス上等間隔になるように配置する。
+ *
+ * @param pathNodes パスを定義する頂点の配列
+ * @param interpolatedPoints パス上に配置する点の数。 1 以上である必要がある。
+ * @param includeLastEndpoint 点を配置するとき、
+ *        {@link pathNodes} の最後の点に重なる点を置くかどうか。
+ *        これを `false` に設定すると、{@link interpolatedPoints} を 1 増やし、
+ *        {@link includeLastEndpoint} を `false` にし、最後の点を削除したのと
+ *        同じ結果が得られる。
+ * @returns パス上等間隔になるように配置された点列
+ */
+export function spreadPointsOverPath(
+  pathNodes: Vector2D[],
   interpolatedPoints: number,
-  isOpenIntervalRight = false
-): Point[] {
-  type SampleWithDistance = SampledPoint & { distance: number };
+  includeLastEndpoint = false
+): E.Either<Error, Vector2D[]> {
+  type NodeWithDistance = Vector2D & { distance: number };
+
+  if (interpolatedPoints < 1)
+    return E.left(
+      new Error(
+        `interpolatedPoints must be positive, got ${interpolatedPoints}`
+      )
+    );
 
   // p1とp2の距離を返す関数
-  const distanceBetween = (p1: SampledPoint, p2: SampledPoint) =>
+  const distanceBetween = (p1: Vector2D, p2: Vector2D) =>
     Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2);
-  // 各samplesのdistance(p_iとp_i+1の距離)を計算する即時関数
-  const samplesWithDistances = (() => {
-    const points: SampleWithDistance[] = [{ ...samples[0], distance: 0.0 }];
+
+  // 各パスノードのdistance(p_iとp_i+1の距離)を計算する即時関数
+  const nodesWithDistances = (() => {
+    const points: NodeWithDistance[] = [{ ...pathNodes[0], distance: 0.0 }];
     let distanceSoFar = 0.0;
-    for (let i = 0; i + 1 < samples.length; i++) {
-      distanceSoFar += distanceBetween(samples[i], samples[i + 1]);
-      points.push({ ...samples[i + 1], distance: distanceSoFar });
+    for (let i = 0; i + 1 < pathNodes.length; i++) {
+      distanceSoFar += distanceBetween(pathNodes[i], pathNodes[i + 1]);
+      points.push({ ...pathNodes[i + 1], distance: distanceSoFar });
     }
     return points;
   })();
+
   const linearlyInterpolate = (
-    p1: SampleWithDistance,
-    p2: SampleWithDistance,
+    p1: NodeWithDistance,
+    p2: NodeWithDistance,
     targetDistance: number
   ) => {
+    if (p1.distance === p2.distance) return p1;
+
     const ratio = (targetDistance - p1.distance) / (p2.distance - p1.distance);
 
     return {
@@ -123,29 +156,30 @@ export function spreadSamplesOver(
     };
   };
 
-  // p_0からp_n-1の総距離
+  // パスをたどった時の総距離
   const totalDistance =
-    samplesWithDistances[samplesWithDistances.length - 1].distance;
-  // 1点辺りの距離
-  const distancePerSample =
-    totalDistance / (interpolatedPoints - (isOpenIntervalRight ? 0 : 1) || 1);
-  let sampleIndexLowerBound = 0;
+    nodesWithDistances[nodesWithDistances.length - 1].distance;
+  // 点を配置するときの1点辺りの距離
+  const distanceCoveredByOneResultPoint =
+    totalDistance / (interpolatedPoints + (includeLastEndpoint ? 1 : 0));
 
-  const result: Point[] = [];
+  const result: Vector2D[] = [];
 
+  let nodeIndexLowerBound = 0;
   for (let i = 0; i < interpolatedPoints; i++) {
-    const targetDistance = distancePerSample * i;
+    const targetDistance = distanceCoveredByOneResultPoint * i;
 
     while (
-      sampleIndexLowerBound < samplesWithDistances.length - 2 &&
-      samplesWithDistances[sampleIndexLowerBound + 1].distance < targetDistance
+      nodeIndexLowerBound < nodesWithDistances.length - 2 &&
+      nodesWithDistances[nodeIndexLowerBound + 1].distance < targetDistance
     ) {
-      sampleIndexLowerBound++;
+      nodeIndexLowerBound++;
     }
 
-    const p1 = samplesWithDistances[sampleIndexLowerBound];
-    const p2 = samplesWithDistances[sampleIndexLowerBound + 1];
+    const p1 = nodesWithDistances[nodeIndexLowerBound];
+    const p2 = nodesWithDistances[nodeIndexLowerBound + 1];
     result.push(linearlyInterpolate(p1, p2, targetDistance));
   }
-  return result;
+
+  return E.right(result);
 }
